5a1d7d9ac471a0f72b92af2f22bb4d56062aeecc
koadjunky <koadjunky@gmail.com>
2019-11-24 12:54:27 +0100

    Heroes and equipment

--- src/shopheroes/google.py
+++ src/shopheroes/google.py
@@ -1,9 +1,7 @@
-import os.path
-libdir = os.path.dirname(os.path.realpath(__file__))
-
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
-
+import os.path
+libdir = os.path.dirname(os.path.realpath(__file__))
 
 
 class ShopHeroesDataSpreadsheet:
@@ -15,6 +13,8 @@ class ShopHeroesDataSpreadsheet:
     def __init__(self):
         keyfile = os.path.join(libdir, 'resources', self.KEYFILE)
         self.credentials = ServiceAccountCredentials.from_json_keyfile_name(keyfile, self.SCOPES)
+        self.gc = None
+        self.sheet = None
         self.connect()
 
     def connect(self):
@@ -51,19 +51,57 @@ class ShopHeroesDataSpreadsheet:
         return result
 
     def get_heroes(self):
+        heroes_wk = self.get_worksheet('Heroes')
+        heroes = heroes_wk.get_all_values()
+
+        powers_wk = self.get_worksheet('Hero Levels & Power')
+        powers = powers_wk.get_all_values()
+
+        result = {}
+        for i in [1, 12, 23]:
+            for j in [1, 9, 17, 25, 33, 41, 49, 57, 65, 73]:
+                record = {
+                    'name': heroes[j][i].lower().title(),
+                    'level': heroes[j][i+3],
+                    'power': 0,
+                    'multi': 0,
+
+                }
+                if record['name'] != '':
+                    result[record['name']] = record
+        for i in range(28):
+            result[powers[i+1][5]]['multi'] = powers[i+1][6]
+            result[powers[i+1][5]]['power'] = powers[i+1][7]
+
+        return result
+
+    def get_equipment(self):
         worksheet = self.get_worksheet('Heroes')
         values = worksheet.get_all_values()
+        result = list()
+        for i in [1, 12, 23]:
+            for j in [1, 9, 17, 25, 33, 41, 49, 57, 65, 73]:
+                result.append(self.get_hero_equipment(i, j, values))
+        return result
 
+    @staticmethod
+    def get_hero_equipment(x, y, values):
         result = []
-        record = {
-            'name': values[1][1]
-        }
-        result.append(record)
-
+        if values[y][x] == '': return result
+        for i in range(7):
+            for j in range(4):
+                if values[y+3+j][x+i] != '---':
+                    record = {
+                        'name': values[y][x].lower().title(),
+                        'type': values[y+2][x+i],
+                        'item': values[y+3+j][x+i],
+                        'affinity': 0,
+                    }
+                    result.append(record)
         return result
 
 
 if __name__ == '__main__':
     import pprint
     ts = ShopHeroesDataSpreadsheet()
-    pprint.pprint(ts.get_heroes())
+    pprint.pprint(ts.get_equipment())
